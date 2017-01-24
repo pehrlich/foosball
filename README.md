335e66b359978742b5f80349f8997ebc1aed7717
mlunoe <michael.lunoe@gmail.com>
2017-01-23 18:15:46 -0800

    Adding simple request for data

--- README.md
+++ README.md
@@ -4,12 +4,14 @@ Mesosphere Foosball
 Install
 -------
 1. `npm install`
-2. `make build`
+2. `npm build`
 
 Develop
 ----------
 1. `npm install`
-2. `make watch`
+2. `npm start`
+3. `cd build`
+4. `python3 -m http.server` or `python -m SimpleHTTPServer`
 
 Use
 ---
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 (function () {
-    console.log('Hello world!');
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', function () {
+    if (xhr.readyState === XMLHttpRequest.DONE && (xhr.status === 200 || this.status === 304)) {
+      console.log(JSON.parse(xhr.response));
+    }
+  });
+
+  // Call API, display result on page
+  xhr.open('GET', './api/users.json', true);
+  xhr.send();
 })();
