62dd73e61b347cb8a72caf0e58f3624ff51cf68c
mlunoe <michael.lunoe@gmail.com>
2017-02-02 19:49:29 -0800

    Update tests

--- src/js/__tests__/example.test.js
+++ src/js/__tests__/example.test.js
@@ -1,13 +1,24 @@
-var assert = require("assert");
+var expect = require('chai').expect;
+var sinon = require('sinon');
 
 describe('main', function () {
 
   beforeEach(function () {
-    this.result = 2 + 1;
+    this.testFunction = function () {
+      return 2 + 1;
+    };
+
+    this.result = this.testFunction();
   })
 
   it('should have dom element', function () {
-    assert.equal(this.result, 3);
+    expect(this.result).equal(3);
+  });
+
+  it('should call testFunction', function () {
+    this.testFunction = sinon.spy();
+    this.testFunction();
+    expect(this.testFunction.called).to.equal(true);
   });
 
 });
