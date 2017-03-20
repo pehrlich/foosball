70dde4e76017cee7298483a356a22d3755e6baba
Peter Ehrlich <peter.i.ehrlich@gmail.com>
2017-03-20 13:21:34 -0700

    Draft of filtering
     - can only click on some ELs

--- src/js/main.js
+++ src/js/main.js
@@ -20,6 +20,43 @@ var getData = function (path, cb) {
 };
 
 
+var userList = [];
+var findUser = function (id) {
+  for (var i = 0; i < userList.length; i++) {
+    if (userList[i].id === id) return userList[i];
+  }
+  return null
+};
+var matchList = [];
+
+
+var printMatches = function (matches) {
+  var dataItem, string = '';
+  if (!matches) matches = matchList;
+
+  for (var i = 0; i < matches.length; i++) {
+    dataItem = matches[i];
+    string += `<a href="match.html" class="match-details">
+                    <div class="match-details__team match-details__team--a is-winner">
+                      <header class="match-details__team__header">Team A</header>
+                      <span class="match-details__team__player">${findUser(dataItem.teamA[0]).name}</span>
+                      <span class="match-details__team__player">${findUser(dataItem.teamA[1]).name}</span>
+                      <span class="match-details__team__points">${dataItem.scoreA}</span>
+                    </div>
+                    <div class="match-details__vs">
+                      <span>VS</span>
+                    </div>
+                    <div class="match-details__team match-details__team--b">
+                      <header class="match-details__team__header">Team B</header>
+                      <span class="match-details__team__player">${findUser(dataItem.teamB[0]).name}</span>
+                      <span class="match-details__team__player">${findUser(dataItem.teamB[1]).name}</span>
+                      <span class="match-details__team__points">${dataItem.scoreB}</span>
+                    </div>
+                  </a>`;
+  }
+  document.getElementById('match__list').innerHTML = string;
+};
+
 var populateUserList = function () {
   'use strict';
 
@@ -27,42 +64,41 @@ var populateUserList = function () {
   getData('./api/users.json', function (data) {
     var string = '';
     for (var i = 0; i < data.length; i++) {
+      userList = data;
       string += `<li class="is-in-lead">
-              <span class="leaderboard__name">${data[i].name}</span>
+              <span class="leaderboard__name" data-match-id="${data[i].id}">${data[i].name}</span>
               <span class="leaderboard__stats leaderboard__stats--wins">${data[i].wins}</span>
               <span class="leaderboard__stats leaderboard__stats--losses">${data[i].losses}</span>
             </li>`;
     }
     document.getElementById('leaderboard__list').innerHTML = string;
+
+    // if RHS already done, refresh here.
+    if (matchList.length > 0) printMatches();
   });
 
   getData('./api/matches.json', function (data) {
-    var string = '';
-    var dataItem;
-    for (var i = 0; i < data.length; i++) {
-      dataItem = data[i];
-      string += `<a href="match.html" class="match-details">
-                  <div class="match-details__team match-details__team--a is-winner">
-                    <header class="match-details__team__header">Team A</header>
-                    <span class="match-details__team__player">${dataItem.teamA[0]}</span>
-                    <span class="match-details__team__player">${dataItem.teamA[1]}</span>
-                    <span class="match-details__team__points">${dataItem.scoreA}</span>
-                  </div>
-                  <div class="match-details__vs">
-                    <span>VS</span>
-                  </div>
-                  <div class="match-details__team match-details__team--b">
-                    <header class="match-details__team__header">Team B</header>
-                    <span class="match-details__team__player">${dataItem.teamB[0]}</span>
-                    <span class="match-details__team__player">${dataItem.teamB[1]}</span>
-                    <span class="match-details__team__points">${dataItem.scoreB}</span>
-                  </div>
-                </a>`;
-    }
-    document.getElementById('match__list').innerHTML = string;
+    matchList = data;
+    printMatches();
   });
 
 };
 
+var selectMatch = function (event) {
+  if (event.target.className != 'leaderboard__name') return
+
+  var id = Number(event.target.dataset.matchId);
+
+  printMatches(
+    matchList.filter(function (m) { return m.id === id })
+  )
+};
+document.addEventListener( 'click', selectMatch );
+
 
 populateUserList();
+
+
+
+
+// todo: don't use className
