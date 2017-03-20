92c5fd971471d7d6ecfcd1357f255f7c8d364d52
Peter Ehrlich <peter.i.ehrlich@gmail.com>
2017-03-20 12:53:21 -0700

    Dynamically show users

--- src/js/main.js
+++ src/js/main.js
@@ -4,15 +4,41 @@ require('../styles/main.scss');
 require('../../api/matches.json');
 require('../../api/users.json');
 
-// TODO: Finish application here
-var xhr = new XMLHttpRequest();
-xhr.addEventListener('load', function () {
-  if (xhr.readyState === XMLHttpRequest.DONE && (xhr.status === 200 || this.status === 304)) {
-    console.log(JSON.parse(xhr.response));
-  }
-});
 
-// Call API, display result on page
-xhr.open('GET', './api/users.json', true);
-xhr.send();
+var getData = function (path, cb) {
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', function () {
+    if (xhr.readyState === XMLHttpRequest.DONE && (xhr.status === 200 || this.status === 304)) {
+      // console.log(JSON.parse(xhr.response));
+      cb(JSON.parse(xhr.response), xhr);
+    }
+  });
 
+  // Call API, display result on page
+  xhr.open('GET', path, true);
+  xhr.send();
+};
+
+
+var populateUserList = function () {
+  'use strict';
+
+  // todo error handling
+  getData('./api/users.json', function (data) {
+    var string = '';
+    for (var i = 0; i < data.length; i++) {
+      // debugger
+      string += `<li class="is-in-lead">
+              <span class="leaderboard__name">${data[i].name}</span>
+              <span class="leaderboard__stats leaderboard__stats--wins">${data[i].wins}</span>
+              <span class="leaderboard__stats leaderboard__stats--losses">${data[i].losses}</span>
+            </li>`;
+    }
+    document.getElementById('leaderboard__list').innerHTML = string;
+    // debugger
+  });
+
+};
+
+
+populateUserList();
